13516d0db8fc956e8e62e91e9b2840562c5c5c6f
gary <gary@garysieling.com>
2017-08-28 20:57:38 -0400

    fixes

---
+++ load_solr.py
@@ -0,0 +1,55 @@
+import sqlite3
+import sys
+import pysolr
+
+solr = pysolr.Solr('http://104.237.151.183:8983/solr/suggestions/', timeout=10)
+solr.delete(q='*:*')
+
+conn = sqlite3.connect('../words.db')
+c = conn.cursor()
+
+def update(data):
+    def getDocument(prefix, row, index):
+        (title, word, suggestions, id) = row
+        doc = {}
+        doc['word'] = ""
+        if (title is None):
+            title = ""
+        if (word is None):
+            word = ""
+
+        if (title == ""):
+            if (word == ""):
+                return ""
+
+            doc['word'] = word
+        else:
+            doc['word'] = title
+
+        doc['suggestions'] = suggestions
+
+        if (suggestions == ""):
+            return ""
+
+        if (suggestions is None):
+            return ""
+
+        shouldCommit = ((index % 1000) == 0)
+
+        doc['id'] = prefix + str(id)
+
+        solr.add([doc], commit=shouldCommit)
+
+    for i, row in enumerate(data):
+        getDocument('a', row, i)
+
+    solr.commit()
+
+update([row for row in c.execute('''SELECT title, word, suggestions, id from dbwords''')])
+
+#[getDocument('b', row) for row in c.execute('''SELECT word as title, word as word, suggestions, id from words''')]
+#solr.commit()
+
+
+
+conn.close()
--- sparql.py
+++ sparql.py
@@ -1,24 +1,30 @@
 import sqlite3
 import sys
-conn = sqlite3.connect('./words.db')
+conn = sqlite3.connect('../words.db')
 
 c = conn.cursor()
 
 from urllib import parse
 def getDescription(id):    
     from SPARQLWrapper import SPARQLWrapper, JSON
-    id = parse.quote_plus(id)
+    id = id.replace("\"", "%22") #parse.quote_plus(id)
+    #print(id)
 
     #id = id.replace("\"", "&quot;")
     sparql = SPARQLWrapper("http://dbpedia.org/sparql")
     query = """
    PREFIX dbres: <http://dbpedia.org/resource/>
-   select ?property ?value where {
+   select ?value where {
    <http://dbpedia.org/resource/"""+id+"""> <http://www.w3.org/2000/01/rdf-schema#label> ?value .
    FILTER(LANG(?value) = "" || LANGMATCHES(LANG(?value), "en"))
 }
 """
-    print(query)
+    
+    try:
+        print(query)
+    except:
+        print("error printing")
+
     sparql.setQuery(query)
 
     sparql.setReturnFormat(JSON)
@@ -35,13 +41,20 @@ def getDescription(id):
 words = [row[0] for row in c.execute('''SELECT word from dbwords WHERE title is null''')]
 for word in words:
     c = conn.cursor()
+
     try:
         print(word) 
-        t = (getDescription(word), word)
-        print(t)
-        c.execute("UPDATE dbwords SET title=? where word=?", t)
     except:
-        print("there was an error",  sys.exc_info())
+        print("error printing word")
+
+    t = (getDescription(word), word)
+
+    try:
+        print(t)
+    except:
+        print("error printing word")
+
+    c.execute("UPDATE dbwords SET title=? where word=?", t)
   
     conn.commit()
     
